fa6ea83aac1571b5e4c23f50d87b821502b10c7c
Erica-Cradeur <79936012+Erica-Cradeur@users.noreply.github.com>
2021-05-09 22:50:25 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -16,5 +16,23 @@ function testJS()
 	var b = document.getElementById('name').value
 
 	document.getElementById('here').innerHTML = b;
+}
 
+function cacheInput(e) {
+    localStorage.setItem(e.attributes["name"].value, e.value)
+}
+
+window.onload = function () {
+    let form = document.getElementById("myForm");
+    let inputs = form.children;
+    for (let i = 0; i < inputs.length; i++) {
+        let el = inputs[i];
+        if (el.tagName.toLowerCase() != "input" || el.attributes["type"].value != "text") {
+            continue
+        }
+        let cachedVal = localStorage.getItem(el.attributes["name"].value)
+        if (cachedVal != null) {
+            el.value = cachedVal;
+        }
+    }
 }
